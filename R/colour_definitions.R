89df6c13be77e6ab2f58907d13af09d702630a93
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2023-05-17 01:10:27 +0100

    add file defining zv colours

---
+++ R/colour_definitions.R
@@ -0,0 +1,134 @@
+#--------------------------------------------------------------------
+
+#' Define global colours
+#' @export
+zv_black <- c("#202020")
+
+#' Define global colours
+#' @export
+zv_white <- c("#FAFAFA")
+
+#' Define global colours
+#' @export
+zv_light_grey <- c("#EEEEEE")
+
+#' Define global colours
+#' @export
+zv_mid_gray <- c("#555555")
+
+#' Define global colours
+#' @export
+zv_orange <- c("#E87800")
+
+#' Define global colours
+#' @export
+zv_teal <- c("#0AB3C7")
+
+#' Define global colours
+#' @export
+zv_fuchia <- c("#C81E87")
+
+#' Define global colours
+#' @export
+zv_blue <- c("#3D52D5")
+
+#' Define global colours
+#' @export
+zv_navy <- c("#003E74")
+
+#--------------------------------------------------------------------
+#' List of colours.
+#'
+#' Complete list of palettes.
+#' Use names(lu_colours) to return all palette names.
+#' @export
+zv_colours <- list(
+  seq_grey = list(c(
+    "#050505", "#0e0e0e", "#181818",
+    "#222222", "#2c2c2c", "#363636",
+    "#3f3f3f", "#494949", "#535353",
+    "#5d5d5d", "#676767", "#707070",
+    "#7a7a7a", "#848484", "#8e8e8e",
+    "#989898", "#a2a2a2", "#ababab",
+    "#b5b5b5", "#bfbfbf", "#c9c9c9",
+    "#d3d3d3", "#dcdcdc", "#e6e6e6",
+    "#f0f0f0", "#fafafa")),
+  seq_orange = list(c(
+    "#100800", "#241300", "#371d00",
+    "#4b2700", "#5f3100", "#723b00",
+    "#864500", "#9a4f00", "#ad5a00",
+    "#c16400", "#d46e00", "#e87800",
+    "#fc8200", "#ff8c10", "#ff9524",
+    "#ff9f37", "#ffa84b", "#ffb25f",
+    "#ffbb72", "#ffc586", "#ffce9a",
+    "#ffd7ad", "#ffe1c1", "#ffead4",
+    "#fff4e8", "#fffdfc")),
+  seq_teal = list(c(
+    "#010b0c", "#021c1f", "#022d32",
+    "#033d44", "#044e57", "#055f6a",
+    "#06707c", "#07818f", "#0891a2",
+    "#09a2b4", "#0ab3c7", "#0bc4da",
+    "#0cd5ec", "#18dcf3", "#2bdff4",
+    "#3ee2f5", "#50e5f6", "#63e8f7",
+    "#76eaf8", "#88edf9", "#9bf0fa",
+    "#aef3fb", "#c1f6fc", "#d3f8fd",
+    "#e6fbfe", "#f9feff")),
+  seq_fuchia = list(c(
+    "#0c0208", "#1d0414", "#2e071f",
+    "#400a2b", "#510c36", "#620f42",
+    "#73114d", "#841459", "#951664",
+    "#a61970", "#b71b7b", "#c81e87",
+    "#d92193", "#e02e9c", "#e23fa4",
+    "#e550ac", "#e761b4", "#ea72bc",
+    "#ec83c4", "#ef94cc", "#f2a5d4",
+    "#f4b6dc", "#f7c7e4", "#f9d8ed",
+    "#fce9f5", "#fefafd")),
+  seq_blue = list(c(
+    "#030510", "#070a20", "#0a1030",
+    "#0e1540", "#111a50", "#151f60",
+    "#182470", "#1c2a80", "#1f2f91",
+    "#2334a1", "#2639b1", "#2a3fc1",
+    "#2d44d1", "#3d52d5", "#4d60d8",
+    "#5d6fdc", "#6d7ddf", "#7d8ce3",
+    "#8e9ae6", "#9ea8ea", "#aeb7ed",
+    "#bec5f1", "#ced3f4", "#dee2f8",
+    "#eef0fb", "#feffff")),
+  seq_navy = list(c(
+    "#000a12", "#001426", "#001f39",
+    "#00294d", "#003460", "#003e74",
+    "#004888", "#00539b", "#005daf",
+    "#0068c2", "#0072d6", "#007dea",
+    "#0087fd", "#1291ff", "#269aff",
+    "#39a3ff", "#4dacff", "#60b5ff",
+    "#74beff", "#88c7ff", "#9bd1ff",
+    "#afdaff", "#c2e3ff", "#d6ecff",
+    "#eaf5ff", "#fdfeff")),
+  set_qual = list(c(
+    "#E87800", "#0AB3C7", "#C81E87",
+    "#3D52D5", "#C0E15C", "#104B6D",
+    "#FFACEC", "#166D2A", "#FE74FE",
+    "#4BE32E", "#A822D9", "#FBD127",
+    "#5A396E", "#DFCFE7", "#5487C0",
+    "#21F0B6"))
+)
+
+## set_20 generated using http://vrl.cs.brown.edu/color
+
+#rgb(232,119,2,maxColorValue = 255)
+#rgb(15,179,200,maxColorValue =  255)
+#rgb(202,31,136,maxColorValue =  255)
+#rgb(60,82,214,maxColorValue = 255)
+#rgb(192,225,92,maxColorValue =  255)
+#rgb(16,75,109,maxColorValue =  255)
+#rgb(255,172,236,maxColorValue =  255)
+#rgb(22,109,42,maxColorValue =  255)
+#rgb(254,116,254,maxColorValue =  255)
+#rgb(75,227,46,maxColorValue =  255)
+#rgb(168,34,217,maxColorValue = 255)
+#rgb(251,209,39,maxColorValue = 255)
+#rgb(90,57,110,maxColorValue = 255)
+#rgb(223,207,231,maxColorValue = 255)
+#rgb(84,135,192,maxColorValue = 255)
+#rgb(33,240,182,maxColorValue = 255)
+
+#--------------------------------------------------------------------
